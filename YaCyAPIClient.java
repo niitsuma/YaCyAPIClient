e2882358084b6a1935cb3ffcfa891849bbdc785c
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2014-07-21 14:03:56 +0900

    EmulateWildcard

--- YaCyAPIClient.java
+++ YaCyAPIClient.java
@@ -1,4 +1,5 @@
-package YaCyAPIClient;
+package yacyapiclient;
+//package YaCyAPIClient;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -13,27 +14,50 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import java.lang.Math.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+//import com.google.guava:guava;
+//import java.util.Collections;
+import java.util.Collection;
+import java.util.AbstractCollection.*;  
+
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableCollection;
+//import com.google.common.collect.
+import com.google.common.collect.Iterables;
+import com.google.common.base.Predicates;
+//import com.google.common.io.
+
 
 public class YaCyAPIClient {
 	private static String YaCy_URL = "localhost:8090";
+	private static int queryRowsEmulateWildcard=100;
+	private static String wildcardSeparator=
+		"\\*"
+		//" \\* "
+		;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(
-				queryExactSentenceToNumFoundStr("This is a")
-				);
-		System.out.println(
-				queryExactSentenceToNumFound("This is a")
-				);
-		ArrayList<String> summaryStringList =
-				queryExactSentenceStartEndToSummaryStringList("This is a",0,3);
-		System.out.println(summaryStringList.get(0));
-				
-	}
+	public static String queryStrToURLString(String queryActual,int start,int rows){
+		//String queryActual=query;
+		//queryActual = queryActual.replaceAll(" ", "%20");
+		//queryActual = queryActual.replaceAll("\"", "%22");
+		String urlStr="http://" 
+						+ YaCy_URL 
+						+ "/solr/collection1/select?q="
+						+ queryActual
+						+ "&defType=edismax";
+		if(start > -1){urlStr = urlStr	+ "&start=" + start;}
+		if(rows > 0  ){urlStr = urlStr 	+ "&rows=" + rows;}
+						//+ "&start=" + start + "&rows=" + rows;
+		return urlStr;}
 	
-	public static Document queryURLToDocument(URL url)
-	{
+	public static Document queryURLToDocument(URL url){
 		Document doc = null;
 		try {
 			//URL url = new URL(url_str);	
@@ -42,12 +66,10 @@ public class YaCyAPIClient {
 			//urlConn.setInstanceFollowRedirects(false);
 			//urlConn.setRequestProperty("Accept-Language", "ja;q=0.7,en;q=0.3");
 			urlConn.connect();
-			
 			//doc = getDocumet(urlConn.getInputStream());
 			DocumentBuilder docbuilder = DocumentBuilderFactory.newInstance()
 					.newDocumentBuilder();
 			doc=docbuilder.parse(urlConn.getInputStream());
-			
 			urlConn.disconnect();
 		} catch (SAXException e) {
 			e.printStackTrace();
@@ -58,25 +80,20 @@ public class YaCyAPIClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return doc;		
-	}
-	public static String queryExactSentenceToNumFoundStr(String aQuery)
-	{
-		String numFoundStr = null;
-		aQuery = aQuery.replaceAll(" ", "%20");
+		return doc;}
+	
+	public static Document queryURLStrToDocument(String urlStr){
+		//System.out.println(urlStr);
 		Document doc = null;
-		String url_str="http://" 
-				+ YaCy_URL 
-				+ "/solr/collection1/select?q=%22"
-				+ aQuery 
-				+ "%22&defType=edismax&start=0&rows=3";
 		try {
-			URL url = new URL(url_str);	
+			URL url = new URL(urlStr);	
 			doc=	queryURLToDocument(url);
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
+			e.printStackTrace();}
+		return doc;}
+	
+	public static String queryDocToNumFoundStr(Document doc){
+		String numFoundStr = null;
 		Element root = doc.getDocumentElement();
 		//System.out.println("root name" + root.getTagName());
 		NodeList resultList = root.getElementsByTagName("result");
@@ -89,26 +106,8 @@ public class YaCyAPIClient {
 		return numFoundStr;
 	}
 	
-	public static int queryExactSentenceToNumFound(String aQuery){
-		return Integer.parseInt(queryExactSentenceToNumFoundStr(aQuery));
-	}
-	
-	public static ArrayList<String> queryExactSentenceStartEndToSummaryStringList(String aQuery,int start, int rows)
-	{
+	public static Collection<String> queryDocToSummaryStringList(Document doc){
 		ArrayList<String> summaryStringList = new ArrayList<String>();
-		aQuery = aQuery.replaceAll(" ", "%20");
-		Document doc = null;
-		String url_str="http://" 
-				+ YaCy_URL 
-				+ "/solr/collection1/select?q=%22"
-				+ aQuery 
-				+ "%22&defType=edismax&start=" + start + "&rows=" + rows;
-		try {
-			URL url = new URL(url_str);	
-			doc=	queryURLToDocument(url);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}		
 		Element root = doc.getDocumentElement();
 		//System.out.println("root name" + root.getTagName());
 		NodeList resultList = root.getElementsByTagName("result");
@@ -135,11 +134,89 @@ public class YaCyAPIClient {
 		    			//System.out.println(strElement.getFirstChild().getNodeValue());
 		    			summaryStringList.add(strElement.getFirstChild().getNodeValue());
 		    		}
-		    		
 		    	}
 		    }
 		}
 		return summaryStringList;
+	}
+
+	public static String queryWildcardSplitedToQueryActual(String[] querySplited){
+		StringBuilder builder = new StringBuilder();			
+		for(String str : querySplited) {
+			//System.out.println(str);
+			//builder.append("\"");
+			builder.append("%22");
+			builder.append(str.replaceAll(" ", "%20"));
+			builder.append("%22");
+			builder.append("%20AND%20");
+		}
+		String queryActual = builder.substring(0, builder.length() - 9);
+		//System.out.println(queryActual);
+		return queryActual;	
+	}
+	
+	public static Collection<String> queryExactSentenceEmulateWildcardToSummaryStringList(String query,int start,int rows){
+		//System.out.println(query);
+		String[] querySplited = query.split(wildcardSeparator);
+		//System.out.println(querySplited.length);
+		//for(String str : queryAry) {System.out.println(str);}		
+		//if(queryAry.length > 1 ){
+			String pat=query.replaceAll("\\*", ".*");
+			//System.out.println(pat);
+			String queryActual = queryWildcardSplitedToQueryActual(querySplited);
+			//System.out.println(queryActual);
+			//ArrayList<String> 
+			Collection<String>
+			summaryStringList0 = 
+				queryDocToSummaryStringList(
+						queryURLStrToDocument(queryStrToURLString(queryActual,start,rows)));
+			//System.out.println(summaryStringList0.size());
+			Collection<String>
+				summaryStringList=
+					Collections2.filter(summaryStringList0,Predicates.containsPattern(pat));
+			//System.out.println(summaryStringList.size());
+			return summaryStringList;
+			//for(String item: summaryStringList){System.out.println("element: " + item);}
+		//}
+	}
+	
+	public static String queryExactSentenceToNumFoundStr(String query){
+		String[] querySplited = query.split(wildcardSeparator);
+		//System.out.println(queryAry.length);
+		//for(String str : queryAry) {System.out.println(str);}		
+		if(querySplited.length > 1 ){
+			int numFund0=Integer.parseInt(
+				queryDocToNumFoundStr(
+					queryURLStrToDocument(
+						queryStrToURLString(
+							queryWildcardSplitedToQueryActual(querySplited)
+					,-1,-1))));
+			int rows=Math.min(numFund0,queryRowsEmulateWildcard);
+			Collection<String> summaryStringList = queryExactSentenceEmulateWildcardToSummaryStringList(query,0,rows);			
+			int numFund1=summaryStringList.size();
+			return String.valueOf(numFund0*numFund1/rows);//Approximated ratio
+		}else{		
+			//String numFoundStr = null;
+			query = query.replaceAll(" ", "%20");
+			String queryActual="%22" + query + "%22";
+			return queryDocToNumFoundStr(
+					queryURLStrToDocument(queryStrToURLString(queryActual,-1,-1)));
+		}	}
+	public static int queryExactSentenceToNumFound(String aQuery){
+		return Integer.parseInt(queryExactSentenceToNumFoundStr(aQuery));}
+	
+	public static Collection<String> queryExactSentenceStartEndToSummaryStringList(String query,int start, int rows){
+		String[] querySplited = query.split(wildcardSeparator);
+		if(querySplited.length > 1 ){
+			return queryExactSentenceEmulateWildcardToSummaryStringList(query,start,rows);			
+		}else{
+			String queryActual="%22" +   query.replaceAll(" ", "%20") + "%22" ;		
+			Collection<String> summaryStringList 
+			//= queryDocToSummaryStringList(doc);
+			= queryDocToSummaryStringList(
+				queryURLStrToDocument(queryStrToURLString(queryActual,start,rows)));
+			return summaryStringList;
+		}
 	}
 	
 	
---
+++ YaCyAPIClientTest.java
@@ -0,0 +1,44 @@
+package yacyapiclient;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import junit.framework.TestCase;
+
+public class YaCyAPIClientTest extends TestCase {
+	public YaCyAPIClientTest(String name) {
+	    super(name);
+	  }
+	 
+	  public void testYaCyAPIClient() {
+		  System.out.println(
+				  YaCyAPIClient.queryExactSentenceToNumFoundStr("This is a")
+					);
+			System.out.println(
+					YaCyAPIClient.queryExactSentenceToNumFound("This is a")
+					);
+			System.out.println(
+					  YaCyAPIClient.queryExactSentenceToNumFoundStr("This * a pen")
+						);
+			System.out.println("---");
+			//ArrayList<String> 
+			Collection<String>
+				summaryStringList =
+					YaCyAPIClient.queryExactSentenceStartEndToSummaryStringList("This is a",1,4);
+			for(String item: summaryStringList){System.out.println("element: " + item);}
+			
+			System.out.println("---");
+			
+			Collection<String>
+			summaryStringList2 =
+			YaCyAPIClient.queryExactSentenceEmulateWildcardToSummaryStringList("This is * pen",2,5);
+			for(String item: summaryStringList2){System.out.println("element: " + item);}
+
+			//YaCyAPIClient.queryExactSentenceEmulateWildcardToSummaryStringList("This is a",-1,-1);
+	
+			//System.out.println(summaryStringList.get(0));
+			//System.out.println(summaryStringList.get(1));
+	    //assertEquals("Error not number type", tn1, test1); 
+	  }
+
+}
